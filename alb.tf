2337f26d6dcf6c67137b942ffe4154f05dab3d64
chitra <chmanu.chinu@gmail.com>
2023-04-15 12:29:14 +0000

    update

--- alb.tf
+++ alb.tf
@@ -1,7 +1,7 @@
 resource "aws_lb" "test" {
   name               = "${var.name}-${var.env}"
   internal           = var.internal
-  load_balancer_type = var.type
+  load_balancer_type = var.load_balancer_type
   subnets            = var.subnets
 
   enable_deletion_protection = var.enable_deletion_protection
